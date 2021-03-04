9901aa22b09c5807e171dd1ce49e4cc3322af7f4
Ikram <60900114006@uin-alauddin.ac.id>
2021-03-04 10:58:00 +0800

    Homepage02

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+// import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -11,169 +11,128 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    // to get size
-    var size = MediaQuery.of(context).size;
-
-    //style
-    var cardTextStyle = GoogleFonts.nunito(
-        fontSize: 15,
-        color: Color.fromRGBO(63, 63, 63, 1),
-        fontWeight: FontWeight.w600);
-
     return Scaffold(
-      body: Stack(
+      body: ListView(
         children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(16.0),
-            height: size.height * .2,
-            decoration: BoxDecoration(
-                // image: DecorationImage(
-                //     alignment: Alignment.topCenter,
-                //     image: AssetImage('assets/images/sunrise.png')),
-                color: Colors.yellow[900],
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(30.0),
-                  bottomRight: Radius.circular(30.0),
-                )),
+          _top(),
+          SizedBox(
+            height: 20.0,
           ),
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    height: 64,
-                    margin: EdgeInsets.only(bottom: 20),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Text(
-                              'Dzikir Pagi dan Petang',
-                              style: GoogleFonts.nunito(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 23,
-                                color: Colors.white,
-                              ),
-                            ),
-                            Text(
-                              "Sesuai as-Sunnah",
-                              style: GoogleFonts.nunito(
-                                fontStyle: FontStyle.italic,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  "Favoritmu",
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22.0),
+                ),
+                Text(
+                  "Lihat semua",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12.0,
                   ),
-                  Expanded(
-                    child: GridView.count(
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,
-                      primary: false,
-                      crossAxisCount: 2,
-                      children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          elevation: 4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/sunrise.png',
-                                  height: 125.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                'Dzikir Pagi',
-                                style: cardTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
-                        Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          elevation: 4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/sunsets.png',
-                                  height: 125.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                'Dzikir Petang',
-                                style: cardTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20.0),
+          Container(
+            height: 200.0,
+            child: GridView(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3, childAspectRatio: 3 / 2),
+              children: <Widget>[
+                _gridItem(Icons.wb_sunny_rounded),
+                _gridItem(Icons.wb_sunny_outlined),
+                _gridItem(Icons.calendar_today),
+                _gridItem(Icons.library_books),
+                _gridItem(Icons.pan_tool_sharp),
+                _gridItem(Icons.image),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-                        Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          elevation: 4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/pray.png',
-                                  height: 125.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                "Do'a-do'a Harian",
-                                style: cardTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
+  _gridItem(icon) {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+          child: Icon(
+            icon,
+            size: 16.0,
+            color: Colors.white,
+          ),
+          backgroundColor: Colors.lightGreen.withOpacity(0.9),
+        ),
+        SizedBox(height: 10.0),
+        Text(
+          "Dzikir Pagi",
+          style: TextStyle(
+            fontSize: 11.0,
+          ),
+        ),
+      ],
+    );
+  }
 
-                        Card(
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(8)),
-                          elevation: 4,
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Container(
-                                child: Image.asset(
-                                  'assets/images/question.png',
-                                  height: 125.0,
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                              Text(
-                                'Tentang',
-                                style: cardTextStyle,
-                              ),
-                            ],
-                          ),
-                        ),
-                        // Container()
-                      ],
-                    ),
+  _top() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+          color: Colors.lightGreen,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(30.0),
+            bottomRight: Radius.circular(30.0),
+          )),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  CircleAvatar(
+                    backgroundImage: AssetImage("assets/images/muslim.png"),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  Text(
+                    "Ahlan wa sahlan",
+                    style: TextStyle(color: Colors.white),
                   ),
                 ],
               ),
-            ),
+              IconButton(
+                icon: Icon(
+                  Icons.notifications,
+                  color: Colors.white,
+                ),
+                onPressed: () {},
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Search",
+                fillColor: Colors.white,
+                filled: true,
+                suffixIcon: Icon(Icons.filter_list),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                  borderSide: BorderSide(color: Colors.transparent),
+                ),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 16.0, vertical: 16.0)),
           ),
         ],
       ),
